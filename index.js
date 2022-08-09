47e133a4e52e30f7895adf20075ee5033f7753a3
Michael Ndigi <michael.ndigi@student.moringaschool.com>
2022-08-09 12:02:47 +0300

    Assignment completed

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+var multiply = num1*num2;
+
+var random = Math.floor(1000 + Math.random() * 9000);
+Number.isInteger(random);
+
+let num3 = 4;
+let num4 = 8;
+var mod = num3%num4;
+
+let max = 20;
